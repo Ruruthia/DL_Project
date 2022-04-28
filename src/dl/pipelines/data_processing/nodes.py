ad9d88278f2f96e8ced6d3f8fe2fab8c8e84db83
Adrian Urbański <adrianu161999@gmail.com>
2022-04-28 18:15:54 +0200

    Merge data_split and data_processing pipelines

--- src/dl/pipelines/data_processing/nodes.py
+++ src/dl/pipelines/data_processing/nodes.py
@@ -3,10 +3,48 @@ This is a boilerplate pipeline 'data_processing'
 generated using Kedro 0.17.7
 """
 
-from typing import Dict, List
+import random
+from copy import copy
+from typing import Dict
+from typing import List
 
+from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader, Subset
 from torchvision import transforms
+from torchvision.datasets import ImageFolder
+
+
+def split(
+        sharks_dataset: ImageFolder,
+        test_ratio: float,
+        val_ratio: float,
+        seed: int,
+) -> Dict[str, Subset]:
+    random.seed(seed)
+
+    train_indices, val_test_indices, _, val_test_targets = train_test_split(
+        range(len(sharks_dataset)),
+        sharks_dataset.targets,
+        stratify=sharks_dataset.targets,
+        test_size=test_ratio + val_ratio,
+        random_state=seed
+    )
+
+    val_indices, test_indices, _, _ = train_test_split(
+        val_test_indices,
+        val_test_targets,
+        stratify=val_test_targets,
+        test_size=test_ratio / (val_ratio + test_ratio),
+        random_state=seed
+    )
+
+    train_sharks_subset = Subset(sharks_dataset, train_indices)
+    val_sharks_subset = Subset(sharks_dataset, val_indices)
+    val_sharks_subset.dataset = copy(sharks_dataset)
+    test_sharks_subset = Subset(sharks_dataset, test_indices)
+    test_sharks_subset.dataset = copy(sharks_dataset)
+
+    return {"train": train_sharks_subset, "val": val_sharks_subset, "test": test_sharks_subset}
 
 
 def process_data(
--- src/dl/pipelines/data_processing/pipeline.py
+++ src/dl/pipelines/data_processing/pipeline.py
@@ -4,11 +4,16 @@ generated using Kedro 0.17.7
 """
 
 from kedro.pipeline import Pipeline, node, pipeline
-from .nodes import process_data
+from .nodes import split, process_data
 
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline([
+        node(
+            func=split,
+            inputs=["sharks_raw_dataset", "params:test_ratio", "params:val_ratio", "params:seed"],
+            outputs="sharks_data_subsets"
+        ),
         node(
             func=process_data,
             inputs=["sharks_data_subsets", "params:batch_size"],
