a4637a9a7edf83e47da618fcbeebb3136eeb907c
Adrian Urbański <adrianu161999@gmail.com>
2022-04-28 18:24:57 +0200

    Reduce repetition of code in test and val transforms

--- src/dl/pipelines/data_processing/nodes.py
+++ src/dl/pipelines/data_processing/nodes.py
@@ -52,37 +52,34 @@ def process_data(
         batch_size: int,
 ) -> List[DataLoader]:
 
+    train_transforms = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(256),
+        transforms.RandomRotation(25),  # randomly rotate images by 25 degrees
+        transforms.RandomHorizontalFlip(),  # randomly flip images horizontally
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.5, 0.5, 0.5],
+            std=[0.5, 0.5, 0.5]
+        )
+    ])
+
+    test_transforms = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(256),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.5, 0.5, 0.5],
+            std=[0.5, 0.5, 0.5]
+        )
+    ]),
+
     data_transforms = {
-        'train': transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(256),
-            transforms.RandomRotation(25),  # randomly rotate images by 25 degrees
-            transforms.RandomHorizontalFlip(),  # randomly flip images horizontally
-            transforms.ToTensor(),
-            transforms.Normalize(
-                mean=[0.5, 0.5, 0.5],
-                std=[0.5, 0.5, 0.5]
-            )
-        ]),
-        'test': transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(256),
-            transforms.ToTensor(),
-            transforms.Normalize(
-                mean=[0.5, 0.5, 0.5],
-                std=[0.5, 0.5, 0.5]
-            )
-        ]),
-        'val': transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(256),
-            transforms.ToTensor(),
-            transforms.Normalize(
-                mean=[0.5, 0.5, 0.5],
-                std=[0.5, 0.5, 0.5]
-            )
-        ]),
+        'train': train_transforms,
+        'test': test_transforms,
+        'val': test_transforms,
     }
+
     for data_set in ('train', 'test', 'val'):
         sharks_data_subsets[data_set].dataset.transform = data_transforms[data_set]
 
